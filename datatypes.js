4ac25ecbec7134569e7bf16d3248dd013b7023be
Emmanuel Bengio <bengioe@gmail.com>
2012-06-18 22:46:17 -0400

    measure to html

--- datatypes.js
+++ datatypes.js
@@ -4,20 +4,68 @@ function Part(){
 }
 
 function Measure(){
+    this.beats = [];
+
+    this.toTabHTML = function(instrument){
+        var strings = new Array();
+        for (var i=0;i<6;i++){strings[i]=new Array();}
+        for (var i=0,len=this.beats.length;
+             i<len;i++){
+            var beat=this.beats[i];
+            for (var j=0,nlen=beat.notes.length;
+                 j<nlen; j++){
+                var note=beat.notes[j];
+                console.log(note.pitch+":"+i);
+                strings[note.string-1][i] = 
+                    note.pitch - instrument.strings[note.string-1];
+            }
+        }
+        var text = "";
+        for (var i=0;i<6;i++){
+            text+="|";
+            for (var j=0;j<this.beats.length;j++){
+                if (strings[i][j]){
+                    text+="-"+strings[i][j];
+                }else{
+                    text+="--";
+                }
+            }
+            text+="-|<br/>";
+        }
+        return text;
+    }
+}
+
+function Beat(){
+    /* The duration, fraction of the measure
+     * a quarter note is 4, a whole note is 1
+     */
+    this.duration = 4;
+    
+    /* the notes of the beat
+     * (normally ordered by pitch)
+     */
     this.notes = [];
 }
 
-function Note(){
+function Note(p,s){
     /* The pitch, midi equivalent
      * C4 == 60
      */
     this.pitch = 60;
-    /* The duration, fraction of the measure
-     * a quarter note is 4, a whole note is 1
-     */
-    this.duration = 4
-    /* The string, as in a stringed instrument
+    if (p!=undefined){
+        this.pitch = p;
+    }
+    /* The string, if in a stringed instrument
      * string 1 is the highest one
      */
-    this.string = 6
+    this.string = 6;
+    if (s!=undefined){
+        this.string = s;
+    }
+}
+
+function StringInstrument(){
+    // EBGDAE
+    this.strings = [88,83,79,74,69,64];
 }
--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 
 function start(){
     document.getElementById("statusbar").innerHTML = "Loading...";
+    var m = new Measure();
+    m.beats.push(new Beat());
+    m.beats.push(new Beat());
+    var instrument = new StringInstrument();
+    m.beats[0].notes.push(new Note(instrument.strings[5]+2,6));
+    m.beats[0].notes.push(new Note(instrument.strings[4]+4,5));
+    m.beats[0].notes.push(new Note(instrument.strings[3]+4,4));
+    m.beats[1].notes.push(new Note(instrument.strings[5]+3,6));
+    m.beats[1].notes.push(new Note(instrument.strings[4]+5,5));
+    m.beats[1].notes.push(new Note(instrument.strings[3]+5,4));
+    var text = m.toTabHTML(instrument);
+    document.getElementById("statusbar").innerHTML = text;
 }
